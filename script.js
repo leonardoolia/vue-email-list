6566f169d53b730d449b9c7e329d3cf07d1c420d
leonardoolia <leonardooliaa@gmail.com>
2024-01-29 14:31:17 +0100

    created email list using a for cycle

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const { createApp } = Vue;
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
 const app = createApp({
+    name: 'Email list',
+    data: () => ({
+        emails: [],
+    }),
+
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get(endpoint).then(res => {
+
+                this.emails.push(res.data.response);
+
+            })
+        }
+    }
+
 
 })
 
